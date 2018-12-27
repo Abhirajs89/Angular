514a08aee24467f93e589999bbe11874ac4badb5
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2018-12-27 13:57:27 -0500

    Dynamically display recipe list using ngFor directive

--- receipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ receipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[] = [
     new Recipe('A Test Recipe','This is simply a test',
+    'https://d2mkh7ukbp9xav.cloudfront.net/recipeimage/cazyuxhz-61b24-382467-cfcd2-bla80i28/46aa8548-4885-4de2-9832-a58f36eac2e4/main/best-ever-garlic-prawns.jpg'),
+    new Recipe('A Test Recipe','This is simply a test',
     'https://d2mkh7ukbp9xav.cloudfront.net/recipeimage/cazyuxhz-61b24-382467-cfcd2-bla80i28/46aa8548-4885-4de2-9832-a58f36eac2e4/main/best-ever-garlic-prawns.jpg')
   ];
 
