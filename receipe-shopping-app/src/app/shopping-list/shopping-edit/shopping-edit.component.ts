c3ec19d13d271684884d5091e1ebdcddd2140237
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2018-12-29 17:32:05 -0500

    Pushed data from shopping edit component to shopping list component

--- receipe-shopping-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ receipe-shopping-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ElementRef, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,8 +12,7 @@ export class ShoppingEditComponent implements OnInit {
    @ViewChild('nameInput') nameInputRef: ElementRef;
    @ViewChild('amountInput') amountInputRef : ElementRef;
 
-   @Output() ingredintAdded = new EventEmitter<Ingredient>();
-  constructor() { }
+  constructor(private slService:ShoppingListService) { }
 
   ngOnInit() {
   }
@@ -21,6 +21,6 @@ export class ShoppingEditComponent implements OnInit {
     const name = this.nameInputRef.nativeElement.value;
     const amount = this.amountInputRef.nativeElement.value;
     const ingredient = new Ingredient(name,amount);
-    this.ingredintAdded.emit(ingredient);
+    this.slService.addIngredint(ingredient);
   }
 }
