f3d4d23e9b1c0c0abf28fcc7a33283ab07bcb326
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2018-12-29 18:00:28 -0500

    Added ingredients to recipes model

--- receipe-shopping-app/src/app/recipes/recipe.service.ts
+++ receipe-shopping-app/src/app/recipes/recipe.service.ts
@@ -1,15 +1,22 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
     
     selectedRecipe = new EventEmitter<Recipe>();
 
     private recipes : Recipe[] = [
-        new Recipe('A Test Recipe','This is simply a test',
-        'https://d2mkh7ukbp9xav.cloudfront.net/recipeimage/cazyuxhz-61b24-382467-cfcd2-bla80i28/46aa8548-4885-4de2-9832-a58f36eac2e4/main/best-ever-garlic-prawns.jpg'),
-        new Recipe('Second Recipe','This is second test',
-        'https://d2mkh7ukbp9xav.cloudfront.net/recipeimage/cazyuxhz-61b24-382467-cfcd2-bla80i28/46aa8548-4885-4de2-9832-a58f36eac2e4/main/best-ever-garlic-prawns.jpg')
+        new Recipe('A Test Recipe',
+        'This is simply a test',
+        'https://d2mkh7ukbp9xav.cloudfront.net/recipeimage/cazyuxhz-61b24-382467-cfcd2-bla80i28/46aa8548-4885-4de2-9832-a58f36eac2e4/main/best-ever-garlic-prawns.jpg',
+        [new Ingredient('Shrimps',20),
+         new Ingredient('White Wine', 1)]),
+        new Recipe('Second Recipe',
+        'This is second test',
+        'https://d2mkh7ukbp9xav.cloudfront.net/recipeimage/cazyuxhz-61b24-382467-cfcd2-bla80i28/46aa8548-4885-4de2-9832-a58f36eac2e4/main/best-ever-garlic-prawns.jpg',
+        [new Ingredient('Meat',200),
+         new Ingredient('Bun', 5)])
       ];
 
       getRecipes(){
